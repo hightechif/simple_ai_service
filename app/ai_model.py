b721e76f33685f7c4697bf54d2778bcdc1a023d0
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2025-11-27 14:16:21 +0700

    FEAT: Use TextBlob

--- app/ai_model.py
+++ app/ai_model.py
@@ -1,18 +1,9 @@
 # ai_model.py
-
-positive_words = ["good", "great", "love", "awesome", "happy", "excellent"]
-negative_words = ["bad", "sad", "terrible", "hate", "awful", "poor"]
+from textblob import TextBlob
 
 def analyze_sentiment(text: str) -> dict:
-    text_lower = text.lower()
-
-    score = 0
-    for w in positive_words:
-        if w in text_lower:
-            score += 1
-    for w in negative_words:
-        if w in text_lower:
-            score -= 1
+    blob = TextBlob(text)
+    score = blob.sentiment.polarity
 
     if score > 0:
         sentiment = "positive"
